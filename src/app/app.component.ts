eb94decb795dd87d31fb7e4a3c04db131c37f382
kwboyd <kwboyd95@gmail.com>
2017-12-30 00:01:31 -0500

    Ripple generally working, cleaning up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { HexModel } from './hexagon/Hex.model';
 })
 export class AppComponent {
   title = 'app';
-  hexagons = [];
+  hexagons: HexModel[] = [];
   
   ngOnInit() {
     console.log('inited');
@@ -20,11 +20,64 @@ export class AppComponent {
     for (let i = 0; i < 36; i++) {
       let column = [];
       for (let j = 0; j < 12; j++) {
-        column.push(new HexModel());
+        column.push(new HexModel(i, j));
       }
       hexagonList.push(column);
     }
     this.hexagons = hexagonList;
     console.log('hexagons made');
+  }
+
+  ripple(hexagon) {
+    hexagon.setStart();
+    let row = hexagon.row;
+    let column = hexagon.column;
+    let offset = row + 1;
+    if (column % 2 === 0) offset = row - 1;
+    let currentColumn = this.hexagons[column];
+    let columnLeft = this.hexagons[column+1];
+    let columnRight = this.hexagons[column-1];
+
+    if (currentColumn[row+1]) currentColumn[row+1].startSecond();
+    if (currentColumn[column][row-1]) currentColumn[column][row-1].startSecond(); 
+    if (columnLeft) {
+      if (columnLeft[row]) columnLeft[row].startSecond();
+      if (columnLeft[offset]) columnLeft[offset].startSecond();
+    }
+    if (columnRight) {
+      if (columnRight[row]) columnRight[row].startSecond();
+      if (columnRight[row]) columnRight[offset].startSecond();
+    }
+
+    let doubleOffset = row;
+    let doubleLeftColumn = this.hexagons[column-2];
+    let doubleRightColumn = this.hexagons[column+2];    
+    if (column % 2 !== 0) doubleOffset = row + 1;    
+    if (this.hexagons[column][row+2]) this.hexagons[column][row+2].startThird();
+    if (this.hexagons[column][row-2]) this.hexagons[column][row-2].startThird(); 
+    if (columnLeft) {
+
+    }
+    if (columnRight) {
+
+    }
+    if (doubleLeftColumn) {
+
+    }
+    if (doubleRightColumn) {
+
+    }
+    this.hexagons[column+1][doubleOffset + 1].startThird();
+    this.hexagons[column+1][doubleOffset - 2].startThird();
+    this.hexagons[column-1][doubleOffset + 1].startThird();
+    this.hexagons[column-1][doubleOffset - 2].startThird();
+    this.hexagons[column-2][row].startThird();
+    this.hexagons[column+2][row].startThird();
+    this.hexagons[column+2][row-1].startThird();
+    this.hexagons[column+2][row+1].startThird();
+    this.hexagons[column-2][row+1].startThird();
+    this.hexagons[column-2][row-1].startThird();
+    
+     
   }
 }
--- src/app/hexagon/Hex.model.ts
+++ src/app/hexagon/Hex.model.ts
@@ -1,7 +1,47 @@
 export class HexModel {
-  color = 'dark';
-  constructor() {
+  color = 'light';
+  column: number;
+  row: number;
+  constructor(column, row) {
+    this.row = row;
+    this.column = column;
+  }
 
+  setStart() {
+    this.color = 'darker';
+    setTimeout(() => {
+      this.setSecond();
+    }, 100)
+  }
+
+  startSecond() {
+    setTimeout(() => {
+      this.setSecond();
+    }, 200)
+  }
+
+  setSecond() {
+    this.color = 'dark';
+    setTimeout(() => {
+      this.setThird();
+    }, 200)
+  }
+
+  startThird() {
+    setTimeout(() => {
+      this.setThird();
+    }, 400)
+  }
+
+  setThird() {
+    this.color = 'medium';
+    setTimeout(() => {
+      this.setFourth();
+    }, 200)
+  }
+
+  setFourth() {
+    this.color = 'light';      
   }
 
 }
